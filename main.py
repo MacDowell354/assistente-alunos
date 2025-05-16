c00204d9cf63755a28082f9eb27ca0e7390ddc19
MacDowell354 <macdowelldacosta@gmail.com>
2025-05-16 13:47:30 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-from fastapi import Request, Form
 
 from gpt_utils import generate_answer
 from search_engine import retrieve_relevant_context
@@ -12,17 +11,14 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
+
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
+
 @app.post("/ask", response_class=HTMLResponse)
 async def ask_question(request: Request, question: str = Form(...)):
     context = retrieve_relevant_context(question)
-   answer = generate_answer(question, context)
-    return templates.TemplateResponse("response.html", {
-        "request": request,
-        "question": question,
-        "answer": answer,
-        "context": context
-    })
+    answer = generate_answer(question, context)
+    return templates.TemplateResponse("response.html", {"request": request, "answer": answer})
